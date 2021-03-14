16cb54c12c13673deb7da4d82870d0d90cc29c18
parv <parvkapadia@gmail.com>
2021-03-14 19:50:28 +0530

    Added a log message

--- src/main/java/com/parvkapadia/springbootjenkinsdemo/SpringBootJenkinsDemoApplication.java
+++ src/main/java/com/parvkapadia/springbootjenkinsdemo/SpringBootJenkinsDemoApplication.java
@@ -8,6 +8,7 @@ public class SpringBootJenkinsDemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootJenkinsDemoApplication.class, args);
+		System.out.println("Starting Spring Boot Application");
 	}
 
 }
